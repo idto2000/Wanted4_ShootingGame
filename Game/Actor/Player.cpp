e7faf68181681fe1180d9859f2831e942299b415
ronniej <ronniej@naver.com>
2026-02-04 16:12:51 +0900

    적 생성/적 탄약 발사 기능 추가
    - Timer 클래스 추가
    - Enemy 클래스 추가 및 이동 / 발사 로직 추가
    - EnemyBullet 클래스 추가

--- Game/Actor/Player.cpp
+++ Game/Actor/Player.cpp
@@ -5,12 +5,16 @@
 #include "Level/Level.h"
 
 Player::Player()
-	: super("<=A=>", Vector2::Zero, Color::Green)
+	: super("<=A=>", Vector2::Zero, Color::Green),
+	fireMode(FireMode::OneShot)
 {
 	// 생성 위치 설정.
 	int xPosition = (Engine::Get().GetWidth() / 2) - (width / 2);
 	int yPosition = Engine::Get().GetHeight() - 2;
 	SetPosition(Vector2(xPosition, yPosition));
+
+	// 타이머 목표 시간 설정.
+	timer.SetTargetTime(fireInterval);
 }
 
 Player::~Player()
@@ -28,6 +32,10 @@ void Player::Tick(float deltaTime)
 		QuitGame();
 	}
 
+	// 경과 시간 업데이트.
+	//elapsedTime += deltaTime;
+	timer.Tick(deltaTime);
+
 	// 좌우 방향키 입력처리.
 	if (Input::Get().GetKey(VK_LEFT))
 	{
@@ -39,9 +47,27 @@ void Player::Tick(float deltaTime)
 	}
 
 	// 스페이스 키를 활용해 탄약 발사.
-	if (Input::Get().GetKey(VK_SPACE))
+	if (fireMode == FireMode::OneShot)
 	{
-		Fire();
+		if (Input::Get().GetKeyDown(VK_SPACE))
+		{
+			Fire();
+		}
+	}
+	else if (fireMode == FireMode::Repeat)
+	{
+		if (Input::Get().GetKey(VK_SPACE))
+		{
+			FireInterval();
+		}
+	}
+
+	// 발사 모드 전환.
+	if (Input::Get().GetKeyDown('R'))
+	{
+		int mode = static_cast<int>(fireMode);
+		mode = 1 - mode;
+		fireMode = static_cast<FireMode>(mode);
 	}
 }
 
@@ -72,9 +98,35 @@ void Player::MoveLeft()
 
 void Player::Fire()
 {
+	// 경과 시간 초기화.
+	//elapsedTime = 0.0f;
+	timer.Reset();
+
 	// 위치 설정.
-	Vector2 bulletPosition(position.x + (width / 2), position.y);
+	Vector2 bulletPosition(
+		position.x + (width / 2), 
+		position.y
+	);
 
 	// 액터 생성.
 	GetOwner()->AddNewActor(new PlayerBullet(bulletPosition));
+}
+
+void Player::FireInterval()
+{
+	// 발사 가능 여부 확인.
+	if (!CanShoot())
+	{
+		return;
+	}
+
+	// 발사.
+	Fire();
+}
+
+bool Player::CanShoot() const
+{
+	// 경과 시간 확인.
+	// 발사 간격보다 더 많이 흘렀는지.
+	return timer.IsTimeOut();
 }
